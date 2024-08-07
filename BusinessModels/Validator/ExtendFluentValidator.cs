72aea5a7ccd0f69dfd923894cf7b56298ee9714f
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-08-07 13:37:21 +0700

    add file

---
+++ BusinessModels/Validator/ExtendFluentValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BusinessModels.Validator;
+
+/// <summary>
+/// Mở rộng tụi Validator cho client
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ExtendFluentValidator<T> : AbstractValidator<T>
+{
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValueAsync => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+        return result.IsValid ? [] : result.Errors.Select(e => e.ErrorMessage);
+    };
+}
