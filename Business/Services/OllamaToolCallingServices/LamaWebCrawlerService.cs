739fdcc621d325da62d1f7b1d0ccb1cf7e8f87e5
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-09-02 12:51:05 +0700

    add webcrawler

---
+++ Business/Services/OllamaToolCallingServices/LamaWebCrawlerService.cs
@@ -0,0 +1,15 @@
+﻿using Abot2.Crawler;
+using Business.Services.OllamaToolCallingServices.Interfaces;
+
+namespace Business.Services.OllamaToolCallingServices;
+
+public class LamaWebCrawlerService : ILamaWebCrawlerService
+{
+    public async Task<string> CrawlAsync(string url, CancellationToken token = default)
+    {
+        var cancelTokenSource = new CancellationTokenSource();
+        var crawler = new PoliteWebCrawler();
+        var result = await crawler.CrawlAsync(new Uri(url), cancelTokenSource);
+        return string.Empty;
+    }
+}
