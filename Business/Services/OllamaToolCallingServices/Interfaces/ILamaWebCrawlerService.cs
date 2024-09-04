e15c876b5e8622320750975500dc02230dca2693
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-09-04 07:32:23 +0700

    update some shiit

--- Business/Services/OllamaToolCallingServices/Interfaces/ILamaWebCrawlerService.cs
+++ Business/Services/OllamaToolCallingServices/Interfaces/ILamaWebCrawlerService.cs
@@ -6,6 +6,6 @@ namespace Business.Services.OllamaToolCallingServices.Interfaces;
 [OllamaTools]
 public interface ILamaWebCrawlerService
 {
-    [Description("collects website data from the provided website")]
+    [Description("collects website data from the provided website url")]
     public Task<string> CrawlAsync([Description("web site to craw")] string url, CancellationToken token = default);
 }
