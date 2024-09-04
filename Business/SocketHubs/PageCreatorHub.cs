37ae98e0dbd0c52038e0c385a3d3a424e014105f
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-09-04 13:37:43 +0700

    static server side render with cache support

---
+++ Business/SocketHubs/PageCreatorHub.cs
@@ -0,0 +1,22 @@
+﻿using BusinessModels.Advertisement;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Business.SocketHubs;
+
+public class PageCreatorHub(IMemoryCache memoryCache) : Hub
+{
+    private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+    public async Task SendMessage(ArticleModel message)
+    {
+        memoryCache.Set($"{nameof(ArticleModel)}{message.Title}", message, new MemoryCacheEntryOptions() { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10) });
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+
+    public async Task GetMessages(string articleId)
+    {
+        var data = memoryCache.Get<ArticleModel>($"{nameof(ArticleModel)}{articleId}");
+        if (data == null) await Clients.Caller.SendAsync("ReceiveMessage", articleId);
+    }
+}
--- BusinessModels/Advertisement/ArticleModel.cs
+++ BusinessModels/Advertisement/ArticleModel.cs
@@ -1,15 +1,24 @@
-﻿using MongoDB.Bson;
+﻿using MessagePack;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace BusinessModels.Advertisement;
 
+[MessagePackObject]
 public class ArticleModel
 {
-    [BsonId] public ObjectId Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Language { get; set; } = string.Empty;
-    public string Content { get; set; } = string.Empty;
-    public string Author { get; set; } = string.Empty;
-    public DateTime PublishDate { get; set; }
-    public string Summary { get; set; } = string.Empty;
+    [Key(0)] [BsonId] public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+    [Key(1)] public string Title { get; set; } = string.Empty;
+    [Key(2)] public string Language { get; set; } = string.Empty;
+    [Key(3)] public string Content { get; set; } = string.Empty;
+    [Key(4)] public string Author { get; set; } = string.Empty;
+    [Key(5)] public DateTime PublishDate { get; set; }
+    [Key(6)] public string Summary { get; set; } = string.Empty;
+
+
+    #region Methods
+
+    public override string ToString() => Title;
+
+    #endregion
 }
