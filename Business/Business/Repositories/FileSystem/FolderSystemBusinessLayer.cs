96ff5b5bd33d4fa0e658a71b3c03cfefe93d1991
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-09-17 15:46:36 +0700

    update

--- Business/Business/Repositories/FileSystem/FolderSystemBusinessLayer.cs
+++ Business/Business/Repositories/FileSystem/FolderSystemBusinessLayer.cs
@@ -112,8 +112,8 @@ public class FolderSystemBusinessLayer(
         if (folder == null) return (false, AppLang.Folder_could_not_be_found);
 
         if (folder.AbsolutePath == "/root") return (false, AppLang.Could_not_delete_root_folder);
-        if(folder.Type == FolderContentType.SystemFolder) return (false, AppLang.Folder_could_not_be_found);
-        
+        if (folder.Type == FolderContentType.SystemFolder) return (false, AppLang.Folder_could_not_be_found);
+
         var res = folderSystemService.Delete(key);
         if (res.Item1)
             foreach (var content in folder.Contents)
@@ -208,11 +208,9 @@ public class FolderSystemBusinessLayer(
 
     public async Task<(bool, string)> CreateFileAsync(FolderInfoModel folder, FileInfoModel file, CancellationToken cancellationTokenSource = default)
     {
-        var newIndex = await GetDocumentSizeAsync(x=>x.Id == folder.Id, cancellationTokenSource);
-        newIndex += 1;
         var dateString = DateTime.UtcNow.ToString("dd-MM-yyy");
 
-        var filePath = Path.Combine(_workingDir, dateString, folder.FolderName, $"_file_{newIndex}.bin");
+        var filePath = Path.Combine(_workingDir, dateString, folder.FolderName, $"_file_{file.Id}.bin");
         Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
 
         file.AbsolutePath = filePath;
@@ -221,7 +219,6 @@ public class FolderSystemBusinessLayer(
         var res = await fileSystemService.CreateAsync(file, cancellationTokenSource);
         if (res.Item1)
         {
-            
             var folderUpdateResult = await UpdateAsync(folder, cancellationTokenSource);
             if (!folderUpdateResult.Item1)
                 return folderUpdateResult;
@@ -286,7 +283,7 @@ public class FolderSystemBusinessLayer(
         request.NewFolder.AbsolutePath = request.NewFolder.RelativePath;
         request.NewFolder.RootFolder = request.RootId;
         request.NewFolder.ModifiedDate = DateTime.Now;
-        
+
 
         if (string.IsNullOrEmpty(request.NewFolder.Username))
             request.NewFolder.Username = folderRoot.Username;
