976a9d9d462450c29dbf1fbfde295df27355f048
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-09-22 13:24:42 +0700

    Update IWeatherService.cs

--- Business/Services/OllamaToolCallingServices/Interfaces/IWeatherService.cs
+++ Business/Services/OllamaToolCallingServices/Interfaces/IWeatherService.cs
@@ -6,13 +6,13 @@ namespace Business.Services.OllamaToolCallingServices.Interfaces;
 [OllamaTools]
 public interface IWeatherService
 {
-    [Description("call to get current weather in a given locatiion (The city and state), e.g. San Francisco")]
+    [Description("call to get current weather in a given locatiion")]
     public Task<string> GetCurrentWeatherAsync([Description("The city and state, e.g. San Francisco, CA")] string location, CancellationToken cancellationToken = default);
 
-    [Description("call to get forecast weather in a given location (The city and state), e.g. San Francisco")]
+    [Description("call to get forecast weather in a given location")]
     public Task<string> GetWeatherForecast([Description("The city and state, e.g. San Francisco, CA")] string location, [Description("Number of days of weather forecast. Value ranges from 1 to 14")] string days, CancellationToken cancellationToken = default);
     
     
-    [Description("call to get location information in a given location")]
+    [Description("call to get location information in a given location like timespan, time zone, etc")]
     public Task<string> GetTimeZoneInfoAsync([Description("US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name")] string location, CancellationToken cancellationToken = default);
 }
