989a0efe992d9ecffa9ece3c160ef2c13521294a
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-10-22 23:12:01 +0700

    Update ImageShowDialog logic to support M3U8 file handling
    Modified the OnParametersSet method to account for M3U8 files within the File.ExtendResource list. If an M3U8 file is found, it sets the ImageUrl accordingly; otherwise, it defaults to handling images and videos as before.

--- WebApp/WebApp.Client/Pages/Drive/View/Dialog/ImageShowDialog.razor.cs
+++ WebApp/WebApp.Client/Pages/Drive/View/Dialog/ImageShowDialog.razor.cs
@@ -1,4 +1,5 @@
-﻿using BusinessModels.System.FileSystem;
+﻿using BusinessModels.General.EnumModel;
+using BusinessModels.System.FileSystem;
 using BusinessModels.Utils;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
@@ -13,13 +14,18 @@ public partial class ImageShowDialog : ComponentBase
     private bool _open;
     private string ImageUrl { get; set; } = string.Empty;
 
-
     protected override void OnParametersSet()
     {
-        if(File.ContentType.IsImageFile())
-            ImageUrl = $"api/files/get-file?id={File.Id.ToString()}";
-        else if(File.ContentType.IsVideoFile())
-            ImageUrl = $"api/files/stream-raid?path={File.Id.ToString()}";
+        if(File.ExtendResource.Any(z => z.Classify == FileClassify.M3U8File))
+            ImageUrl = $"api/files/get-file?id={File.ExtendResource.FirstOrDefault(z=>z.Classify == FileClassify.M3U8File)}";
+        else
+        {
+            if (File.ContentType.IsImageFile())
+                ImageUrl = $"api/files/get-file?id={File.Id.ToString()}";
+            else if (File.ContentType.IsVideoFile())
+                ImageUrl = $"api/files/stream-raid?path={File.Id.ToString()}";
+        }
+        
         base.OnParametersSet();
     }
 
