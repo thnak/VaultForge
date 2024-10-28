0ed94ef53c47ea384e54581cd5f93eb227f9a914
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-10-28 17:20:12 +0700

    Add logging for MongoDB connection details
    Introduced a logger to MongoDataLayerContext to log MongoDB configuration details, ensuring transparency and ease of debugging. The log includes connection string, database name, username, and password.

--- Business/Data/Repositories/MongoDataLayerContext.cs
+++ Business/Data/Repositories/MongoDataLayerContext.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using Business.Data.Interfaces;
 using BusinessModels.General.SettingModels;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Configuration;
@@ -8,15 +10,22 @@ namespace Business.Data.Repositories;
 
 public class MongoDataLayerContext : IMongoDataLayerContext
 {
-    public MongoDataLayerContext(IOptions<AppSettings> settings)
+    public MongoDataLayerContext(IOptions<AppSettings> settings, ILogger<IMongoDataLayerContext> logger)
     {
         var dbName = settings.Value.DbSetting.DatabaseName;
         var user = settings.Value.DbSetting.UserName;
         var pass = settings.Value.DbSetting.Password;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine("MongoDB configuration:");
+        stringBuilder.AppendLine($"- connectionString: {settings.Value.DbSetting.ConnectionString}");
+        stringBuilder.AppendLine($"- dbName: {dbName}");
+        stringBuilder.AppendLine($"- user: {user}");
+        stringBuilder.AppendLine($"- pass: {pass}");
+        logger.LogInformation(stringBuilder.ToString());
 
         MongoIdentity identity = new MongoInternalIdentity("admin", user);
         MongoIdentityEvidence evidence = new PasswordEvidence(pass);
-        
+
         var setup = new MongoClientSettings
         {
             Scheme = ConnectionStringScheme.MongoDB,
