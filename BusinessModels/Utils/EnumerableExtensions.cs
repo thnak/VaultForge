065b1b471ace817ef5b9766e03588d47fc3fa22e
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-12-01 17:36:02 +0700

    shit

---
+++ BusinessModels/Utils/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace BusinessModels.Utils;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> WhereWithCancellation<T>(this IEnumerable<T> source, Func<T, bool> predicate, CancellationToken cancellationToken = default)
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+        foreach (var item in source.TakeWhile(_ => !cancellationToken.IsCancellationRequested))
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+}
