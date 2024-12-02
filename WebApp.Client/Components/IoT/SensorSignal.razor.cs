c0cc01c92778fdf4b08001521692889222a59e18
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-12-02 16:21:15 +0700

    auto rejoin the group when possible

--- WebApp.Client/Components/IoT/SensorSignal.razor.cs
+++ WebApp.Client/Components/IoT/SensorSignal.razor.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessModels.Utils;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR.Client;
+using WebApp.Client.Utils;
 
 namespace WebApp.Client.Components.IoT;
 
@@ -20,14 +21,17 @@ public partial class SensorSignal(ILogger<SensorSignal> logger) : ComponentBase,
             var cancellationToken = CancellationTokenSource.Token;
             var value = await ApiService.GetAsync<ulong>($"/api/iot/v1/get-count/{SensorId}", cancellationToken);
             if (value.IsSuccessStatusCode)
-                { 
+            {
                 CountValue = value.Data;
                 await InvokeAsync(StateHasChanged);
             }
+
             HubConnection = new HubConnectionBuilder().InitConnection(Navigation.BaseUri + "hubs/iotSensor");
             HubConnection.On<ulong>("ReceiveCount", ShowValue);
             HubConnection.On<float>("ReceiveValue", ReceiveValue);
             HubConnection.On<string>("ReceiveMessage", ReceiveMessage);
+            HubConnection.Reconnected += HubConnectionOnReconnected;
+            HubConnection.Reconnecting += HubConnectionOnReconnecting;
             await HubConnection.StartAsync(cancellationToken);
             EventListener.PageHideEventAsync += PageHideEvent;
             EventListener.PageShowEventAsync += PageShowEvent;
@@ -35,6 +39,25 @@ public partial class SensorSignal(ILogger<SensorSignal> logger) : ComponentBase,
         }
 
         await base.OnAfterRenderAsync(firstRender);
+    }
+
+    private Task HubConnectionOnReconnecting(Exception? arg)
+    {
+        if (arg is not null)
+        {
+            ToastService.ShowError(arg.Message, TypeClassList.ToastDefaultSetting);
+            return Task.CompletedTask;
+        }
+
+        ToastService.ShowSuccess("Connecting..", TypeClassList.ToastDefaultSetting);
+        return Task.CompletedTask;
+    }
+
+    private Task HubConnectionOnReconnected(string? arg)
+    {
+        ToastService.ShowSuccess($"Reconnected to Hub: {arg}", TypeClassList.ToastDefaultSetting);
+        if (HubConnection != null) return HubConnection.InvokeAsync("JoinSensorGroup", SensorId);
+        return Task.CompletedTask;
     }
 
     private Task ReceiveValue(float arg)
@@ -75,6 +98,7 @@ public partial class SensorSignal(ILogger<SensorSignal> logger) : ComponentBase,
     {
         if (HubConnection != null)
         {
+            HubConnection.Reconnected -= HubConnectionOnReconnected;
             await HubConnection.InvokeAsync("LeaveSensorGroup", SensorId);
             logger.LogInformation($"Disposing ServerTime {ElementId}");
             await HubConnection.DisposeAsync();
