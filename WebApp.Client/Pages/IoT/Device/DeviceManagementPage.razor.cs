e729eed1c823b94f7d94d815616a4c8732aef032
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-12-30 23:03:17 +0700

    add page size

--- WebApp.Client/Pages/IoT/Device/DeviceManagementPage.razor.cs
+++ WebApp.Client/Pages/IoT/Device/DeviceManagementPage.razor.cs
@@ -35,7 +35,7 @@ public partial class DeviceManagementPage : ComponentBase, IDisposable
 
     private async Task<GridData<PageModel>> ServerReload(GridState<PageModel> arg)
     {
-        var result = await ApiService.GetAsync<SignalrResultValue<IoTDevice>>("api/device/get-device");
+        var result = await ApiService.GetAsync<SignalrResultValue<IoTDevice>>($"api/device/get-device?page{arg.Page}&pageSize={arg.PageSize}");
         return new GridData<PageModel>
         {
             Items = result.Data?.Data.Select(x => new PageModel(x)
